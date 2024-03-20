ce9f59cbe3931bed290c0dc435fbbfad3ee9aaa0
Oebelysk <bandy.bryce@gmail.com>
2024-03-20 01:31:48 -0400

    Testing initialization

--- player/Player.gd
+++ player/Player.gd
@@ -30,7 +30,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# Get the input direction and handle the movement/deceleration mechanics.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
