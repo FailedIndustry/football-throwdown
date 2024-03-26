22926fb53f8f4a5af201ab7605c2cefe6d0e7920
Oebelysk <bandy.bryce@gmail.com>
2024-03-26 00:02:23 -0400

    Added elite voice acting

--- EventHandler.gd
+++ EventHandler.gd
@@ -2,15 +2,28 @@ extends Node3D
 
 var red_score : int = 0
 var blue_score : int = 0
+var oyelly_score : int = 0
 
 @onready var score_counter = $"../Player/ScoreCounter"
+@onready var blue_goal_announce = $"BlueGoalAnnounce"
+@onready var red_goal_announce = $"RedGoalAnnounce"
+@onready var oyelly_goal_announce = $"OyellyGoalAnnounce"
 
 func _on_blue_goal_body_entered(body):
 	print("Blue goal entered.")
 	blue_score += 1
 	score_counter.set_score_blue(blue_score)
+	blue_goal_announce.play()
 
 func _on_red_goal_body_entered(body):
 	print("Red goal entered.")
 	red_score += 1
 	score_counter.set_score_red(red_score)
+	red_goal_announce.play()
+
+
+func _on_oyelly_goal_body_entered(body):
+	print("Oyelly Company goal entered.")
+	oyelly_score += 1
+	score_counter.set_score_oyelly(oyelly_score)
+	oyelly_goal_announce.play()
--- gui/ScoreCounter.gd
+++ gui/ScoreCounter.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var red_team_score = $RedTeamScore
 @onready var blue_team_score = $BlueTeamScore
+@onready var oyelly_team_score = $OyellyTeamScore
 
 func set_score_red(new_score):
 	red_team_score.text = str(new_score)
@@ -9,3 +10,5 @@ func set_score_red(new_score):
 func set_score_blue(new_score):
 	blue_team_score.text = str(new_score)
 
+func set_score_oyelly(new_score):
+	oyelly_team_score.text = str(new_score)
